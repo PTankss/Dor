d45f09058f2f026de72e7c36f06d7db16271cfb4
Elias <111512975+PTankss@users.noreply.github.com>
2023-07-28 21:51:04 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,7 +61,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //messages
 global.mess = {
-    success: 'Here you go B*tch!', 
+    success: 'Here you go!    btw https://youtube.com/@DorTurkyy', 
 }
 
 let file = require.resolve(__filename)
